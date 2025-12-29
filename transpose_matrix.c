374489440a19a43d017f33c34424c8f6cce90522
Himel <foisalhimel@gmail.com>
2025-12-30 00:17:57 +0600

    Transpose matrix

---
+++ transpose_matrix.c
@@ -0,0 +1,58 @@
+#include<stdio.h>
+
+int main()
+{
+    int A[10][10], transpose[10][10], i, j, row, col;
+
+    printf("Enter the row and coloum of the matrix: ");
+    scanf("%d %d",&row,&col);
+
+    //input matrix
+
+    for(i=0; i<row; i++)
+    {
+        for(j=0; j<col; j++)
+        {
+            printf("A[%d][%d] = ",i,j);
+            scanf("%d",&A[i][j]);
+
+        }
+    }
+
+    //for transpose matrix
+
+    for(i=0; i<row; i++)
+    {
+        for(j=0; j<col; j++)
+        {
+            transpose[j][i] = A[i][j] ;
+        }
+    }
+
+    //print A Matrix
+    
+    printf("\nMatix of A =\n");
+    for(i=0; i<row; i++)
+    {
+        for(j=0; j<col; j++)
+        {
+            printf("%d\t",A[i][j]);
+
+        }
+
+        printf("\n");
+    }
+
+    printf("\nTranspose of Matrix A = \n");
+    
+     for(i=0; i<col; i++)
+    {
+        for(j=0; j<row; j++)
+        {
+            printf("%d\t",transpose[i][j]);
+
+        }
+        printf("\n");
+    }
+
+}
