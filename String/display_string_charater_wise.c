68653be8982736865541ae6c3fef6ec0d22f52cf
Himel <foisalhimel@gmail.com>
2025-12-31 01:31:39 +0600

    Initial Commit

---
+++ String/display_string_charater_wise.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+   char Name[] = "FAYSAL HIMEL";
+
+   int i;
+   i=0;
+   while(Name[i]!='\0')
+   {
+    printf("%c\n",Name[i]);
+
+    i++;
+    
+   }
+   return 0;
+
+}
---
+++ String/string.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+    //initialize String 
+     char name[] ={'H','I','M','E','L','\0'} ;   //je koita Character thakbe tar theke ekta beshi index nibo
+
+     printf("%s",name);
+
+     return 0;
+
+
+}
